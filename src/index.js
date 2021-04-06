a4e6878ffc480e98b74284a87dd01fa6a05e3ad9
alisa1649 <alisa1649@gmail.com>
2021-04-06 18:06:03 -0400

    Added modal styling

--- src/index.js
+++ src/index.js
@@ -2,28 +2,33 @@ import "./styles/index.scss";
 let score = 0;
 let history = [];
 
+// =============================================
+// Game Grid Animation
+// =============================================
+
 function addActiveClass(gridIndex) {
   const previous = document.querySelector(".grid-cell.active")
   if (previous) {
     previous.classList.remove("active");
   }
-
-
   const active = document.querySelector(`.grid-cell-${gridIndex}`);
   window.setTimeout(() => {
     active.className += " active"
     history.push(gridIndex);
   }, 1000)
-
 }
 
-
-
+// Adds active class to random grid cell every X seconds
 window.setInterval(() => {
   const gridIndex = Math.floor(Math.random() * 2);
   addActiveClass(gridIndex)
 }, 2000)
 
+// =============================================
+// Player Input
+// =============================================
+
+// Listen for match button click
 const button = document.querySelector("#match-button");
 button.addEventListener("click", () => {
   const success = document.querySelector(".grid-container");
@@ -35,12 +40,19 @@ button.addEventListener("click", () => {
       success.classList.remove("correct")
     }, 1000)
   }
-
 })
 
+// =============================================
+// Instructions Modal Open and Close
+// =============================================
+
+
+// display none to modal close
+// click listener on modal screen and "close" button on modal to close
+//
 
 
 
 
-// Invoke compareCells on click, update score and render green or red grid outline (in index.html for now )
-// <button onClick={() => compareCells()}></button>
+
+
