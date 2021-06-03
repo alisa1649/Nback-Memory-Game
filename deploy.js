d547452399171fd38ae3f39e9f0eeceac28bf58c
alisa1649 <alisa1649@gmail.com>
2021-06-03 14:18:04 -0400

    trying to fix favicon

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish('./', {
-    src: ["dist/**/*", "index.html"],
+    src: ["dist/**/*", "index.html", "assets/*"],
 }, function(err) {console.log(err)});
